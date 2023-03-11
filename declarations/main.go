d0da038dc25cb002dd3fd90a59dba1e5ec13d800
Luiz Henrique <luizhjramos@outlook.com>
2023-03-11 14:38:40 -0300

    feat: declarations studies

---
+++ declarations/main.go
@@ -0,0 +1,15 @@
+package main
+
+const a = "hello-world"
+
+var (
+	b bool   = true
+	c int    = 30
+	d string = "Luiz"
+	e float64
+)
+
+func main() {
+	f := "short var declaration"
+	println(f)
+}
